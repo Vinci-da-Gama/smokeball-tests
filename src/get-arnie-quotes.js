c20041776e98874765971bcfa7fde8d797960c80
Emma Aia <emmaaia@192-168-1-102.tpgi.com.au>
2021-03-02 15:15:15 +1100

    git comments

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -2,16 +2,43 @@
 const { httpGet } = require("./mock-http-interface");
 
 /**
- * @param {urls} urls -- string array
+ * @param {string[]} urls -- The urls to be requested
+ * @return [{ ["Arnie Quote" || "FAILURE"]: message }] results -- Array of objects
  * ust getArnieQuotes method to get data from mock urls.
  */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
+  // TODO: Implement this function -- done
+  /**
+   * use Promise.all to handle permise one by one,
+   * then return results
+   */
   const results = await Promise.all(
+    /**
+     * use map method to generate results array
+     * the async fat arrow function will handle each http call
+     */
     urls.map(async (url) => {
+      /**
+       * try catch block for await http call
+       */
       try {
+        /**
+         * get http data, and then assign to a const variable,
+         * and then expose keys, so it would be a bit easier for
+         * further usage
+         */
         const { status, body } = await httpGet(url);
+        /**
+         * use JSON.parse to convert stringified object,
+         * and then get message value, and then assign to const message
+         */
         const message = JSON.parse(body).message;
+        /**
+         * if status === 200
+         * @return [{ ["Arnie Quote"]: message }]
+         * else
+         * @return [{ ["FAILURE"]: message }]
+         */
         if (status === 200) {
           return { ["Arnie Quote"]: message };
         } else {
