a34ee21258a7fd434b9bfe9d594210f676f03e8c
Emma Aia <emmaaia@192-168-1-102.tpgi.com.au>
2021-03-02 14:57:05 +1100

    update the method

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,37 @@
-const { httpGet } = require('./mock-http-interface');
+// import mock httpGet method to perform HTTP requests
+const { httpGet } = require("./mock-http-interface");
 
+/**
+ * @param {urls} urls -- string array
+ * ust getArnieQuotes method to get data from mock urls.
+ */
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
+  const results = await Promise.all(
+    urls.map(async (url) => {
+      try {
+        const { status, body } = await httpGet(url);
+        const message = JSON.parse(body).message;
+        if (status === 200) {
+          return { ["Arnie Quote"]: message };
+        } else {
+          return { ["FAILURE"]: message };
+        }
+      } catch (error) {
+        /**
+         * If has an error, then printout the error for now.
+         */
+        console.log("24 -- error: ", error.message);
+      }
+    })
+  );
   // return results;
+  return results;
 };
 
+/**
+ * export method: getArnieQuotes
+ */
 module.exports = {
   getArnieQuotes,
 };
